dc647ef1541cc1284f7c4672dedf91430b6f39cd
DESKTOP-O5C9KDO\diana <A01167418@itesm.mx>
2017-09-06 11:42:06 -0500

    Ejercicios de Clase- triangulo

--- main.cpp
+++ main.cpp
@@ -10,175 +10,140 @@ Autor: Diana Laura Valencia Dominguez A01167418
 //  Created by Diana Valencia  on 2017-08-14.
 //
 //
-
 #include <iostream>
-using namespace std;
+#include <GL/glew.h>
+#include<GL/freeglut.h>
+#include <glm/glm.hpp> //libreria de matematicas de open gl
+#include <vector> //libreria de c++ una lista de elementos
 
-class Ejercicios1 {
-public:
-	// Perimetro rectangulo
-	int PerimetroRectangulo(int base, int altura) {
-		int p = (base + altura) * 2;
-		return p;
-	}
-public:
-	// Area rectanculo
-	float AreaRectangulo(float base, float altura) {
-		float a = (base *  altura) / 2;
-		return a;
-	}
-public:
-	// Numero mayor
-	int Mayor(int n1, int n2, int n3) {
-		if (n1>n2 && n1>n3) {
-			return n1;
-		}
-		else if (n2>n1 && n2>n3) {
-			return n2;
-		}
-		else if (n3>n1 && n3>n2) {
-			return n3;
-		}
-	}
-public:
-	// Numero menor
-	int Menor(int n1, int n2, int n3) {
-		if (n1<n2 && n1<n3) {
-			return n1;
-		}
-		else if (n2<n1 && n2<n3) {
-			return n2;
-		}
-		else if (n3<n1 && n3<n2) {
-			return n3;
-		}
-	}
-public:
-	// Fila estrellas
-	void FilaEstrellas(int n) {
-		int cont = 0;
-		while (cont <= n) {
-			cout << "*";
-			cont++;
-		}
-	}
-public:
-	// Matiz estrellas
-	void MatrizEstrellas(int n) {
-		int cont, cont2;
-		for (cont = 0; cont <= n; cont++) {
-			cout << "\n";
-			cont2 = 0;
-			while (cont2 <= n) {
-				cout << "*";
-				cont2++;
-			}
-		}
-	}
-public:
-	//Piramide estrellas
-	void PiramideEstrellas(int n) {
-		int cont = 0, cont2 = 0;
-		for (cont; cont < n; cont++) {
-			cont2 = 0;
-			while (cont2 <= cont) {
-				cout << "*";
-				cont2++;
+//Identificador de manager de VBOs para una geometria (conjunto de vertices)
+//nunca hacer, es malo jiji 
+GLuint vao;
 
-			}cout << "\n";
-		}
-	}
+//Función que va a inicializar toda la memoria del programa
+void Initialize()
+{
+	//posiciones del triángulo
+	//vamos a crear una lista que va a almacenar las posiciones 
+	//en dos dimensiones de un triangulo 
+	//Esto es en CPU y RAM
+	std::vector<glm::vec2> positions;
+	//en sentido contrario de las manecillas del reloj
+	positions.push_back(glm::vec2(-1.0f, -1.0f));
+	positions.push_back(glm::vec2(1.0f, -1.0f));
+	positions.push_back(glm::vec2(0.0f, 1.0f));
 
-public:
-	//Piramide estrellas
-	void FlechaEstrellas(int n) {
-		int cont = 0, cont2 = 0;
-		for (cont; cont < n; cont++) {
-			cont2 = 0;
-			while (cont2 <= cont) {
-				cout << "*";
-				cont2++;
+	std::vector<glm::vec3> colors;
+	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
+	colors.push_back(glm::vec3(0.0f, 1.0f, 0.0f));
+	colors.push_back(glm::vec3(0.0f, 0.0f, 1.0f));
 
-			}cout << "\n";
-		}
-
-		for (cont = n-1; cont >= 0; cont--) {
-			cont2 = cont;
-			while (cont2 > 0) {
-				cout << "*";
-				cont2--;
-
-			}cout << "\n";
-		}
-	}
-public:
-	// Sucesión Fibonacci
-	void Fibonacci(int n) {
-		int num1 = 1, num2 = 1, suma=1, r;
-		cout << num1;
-		for (r = 1; r < n; r++) {
-			cout << " " << suma;
-			suma = num1 + num2;
-			num1 = num2;
-			num2 = suma;
-		}
-		cout << "\n";
-		}
-public:
-	// Numero Primo
-	bool EsPrimo(int n) {
-		if (n == 2 || n == 3) {
-			cout << "true" << "\n";
-			return true;
-		}if (n % 2 != 0 && n % 3 != 0) {
-			cout << "true" << "\n";
-			return true;
-		}
-		else cout << "false" << "\n";
-		return false;
-	}
-};
-int main() {
-	Ejercicios1 ej1;
-	cout << "El perimetro es: \n";
-	int perimetro = ej1.PerimetroRectangulo(5, 3);
-	cout << perimetro;
-
-	cout << "\nEl area es: \n";
-	int area = ej1.AreaRectangulo(5.0f, 3.0f);
-	cout << area;
-
-	cout << "\nEl numero mayor es: \n";
-	int mayor = ej1.Mayor(5, 9, 1);
-	cout << mayor;
-
-	cout << "\nEl numero menor es: \n";
-	int menor = ej1.Menor(5, 9, 1);
-	cout << menor;
-
-	cout << "\n";
-	ej1.FilaEstrellas(5);
-
-	cout << "\n";
-	ej1.MatrizEstrellas(4);
-
-	cout << "\n";
-	ej1.PiramideEstrellas(6);
-
-	cout << "\n";
-	ej1.FlechaEstrellas(3);
-
-	cout << "\n Sucesion Fibonacci: \n";
-	ej1.Fibonacci(9);
-
-	cout << "\n Es primo: \n";
-	ej1.EsPrimo(79);
-
-	cout << "\n Es primo:\n";
-	ej1.EsPrimo(52);
+	//creamos un VAO y almacenamos el id en la varible vao.
+	//este es el manager de VBOs
+	glGenVertexArrays(1, &vao);
+	//le decimmos a OpenGL que lo queremos utilizar
+	//todos los VBOs que creemos a partir de ahora y hasta que lo desactivemos (este VAO), se van a asociar a este manager
+	glBindVertexArray(vao);
 
 
+	//donde guardar el identificador
+	//equivale a un entero sin signo : unsigned int numero entero
+	//identificador de VBO de posiciones. aqui lo vamos a almacenar
+	GLuint positionsVBO;
+	//creamos un VBO y almacenamos el id en la variable positionsVBO
+	// &: mandar variable como referencia 
+	glGenBuffers(1, &positionsVBO);
+	//le decimos a OpenGL que lo queremos utilizar como un atributo (GL_ARRAY_BUFFER)
+	glBindBuffer(GL_ARRAY_BUFFER, positionsVBO);
+	//asignamoslos datos al buffer y los mandamos a la tarjeta de video -> guardar una sola vez y leerlo muchas veces (GL_STATIC_DRA)
+	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * positions.size(), positions.data(), GL_STATIC_DRAW);
+	//quiero activar el atributo con indice 0 en la tarjeta de video (un arreglo y el primero como un atributo)
+	glEnableVertexAttribArray(0);
+	//configuramos el atributo con indice 0 en la tarjeta de video (cada dos elementos hacen un atributo, son un float)
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+	//desactivar el uso de VBO de positions
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	std::cin.get();
+	GLuint colorsVBO;
+	glGenBuffers(1, &colorsVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, colorsVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * colors.size(), colors.data(), GL_STATIC_DRAW);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
+	//ya no quiero utilizar el VAO, los VBOs ya no se asocian con este VAO
+	glBindVertexArray(0);
+
+
 }
 
+void MainLoop()
+{
+	// Borramos el buffer de color y profundidad siempre al inicio de un nuevo frame.
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// TODO: hacer el render
+	//Paso 1: no se puede hacer aun
+	//Paso 2: Activar un VAO
+	glBindVertexArray(vao);
+	
+	//Paso 3: dibugamos
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	//Paso 4: desactivamos el VAO
+
+	glBindVertexArray(0);
+
+	//Paso 5: aun no se puede hacer
+
+
+	// Intercambiar los buffers (el que se estaba rendereando con el que se estaba
+	// mostrando).
+	glutSwapBuffers();
+}
+
+int main(int argc, char* argv[])
+{
+	// Inicializamos freeglut. 
+	// Freeglut se encarga de generar y manejar ventanas.
+	// Utilizamos freeglut para crear una ventana donde vamos a dibujar.
+	glutInit(&argc, argv);
+	// Tenemos que informar que queremos trabajar con el pipeline
+	// clásico (OpenGL viejito).
+	glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+	// Freeglut nos permite configurar eventos que ocurren en la ventana.
+	// Un evento que nos interesa es cuando alguien cierra la ventana.
+	// En este caso, simplemente dejamos de renderear y terminamos el programa.
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+	// Configuramos el framebuffer. En este caso estamos solicitando un buffer
+	// de color true color RGBA, un buffer de profundidad y un segundo buffer 
+	// para rendererar (evitamos parpadeos al renderear).
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+	// Solicitamos una ventana de 400x400 pixeles.
+	glutInitWindowSize(400, 400);
+	// Creamos y abrimos la ventana con un título personalizado.
+	glutCreateWindow("Hello World OpenGL");
+	// Asociar una función de render. Esta función se mandará a llamar para 
+	// dibujar un frame.
+	glutDisplayFunc(MainLoop);
+
+	// Inicializamos GLEW. Esta librería se encarga de obtener el API
+	// de OpenGL de nuestra tarjeta de video. SHAME ON YOU MICROSOFT.
+	glewInit();
+
+	// COnfigurar OpenGL. Este es el color por default del buffer de color
+	// en el framebuffer.
+	glClearColor(1.0f, 1.0f, 0.5f, 1.0f);
+
+	//configurar la memoria que la aplicacion va a necesitar 
+	Initialize();
+
+	// Iniciamos la aplicacion gráfica. El main se pausará en estea línea
+	// hasta que se cierre la ventana de OpenGL.
+	glutMainLoop();
+
+	return 0;
+}
